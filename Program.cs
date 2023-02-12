e6f8ac756d7888d8cf8d955a74d0b02fe087f74f
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-12 23:00:54 +0500

    add dr4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 2)
+{
+    System.Console.WriteLine("Wrong number");
+}
+for (int i = 2; i <= n; i+=2)
+{
+    System.Console.Write($"{i} ");
+}
